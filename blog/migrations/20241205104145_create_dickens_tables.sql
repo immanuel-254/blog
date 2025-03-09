86a90ff35b49a0f4931a3437274643537b039357
immanuel-254 <njugunaimmanuel1@outlook.com>
2025-03-09 12:22:50 +0300

    added publish field on Blogs table and added a Comments table

--- blog/migrations/20241205104145_create_dickens_tables.sql
+++ blog/migrations/20241205104145_create_dickens_tables.sql
@@ -2,25 +2,39 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS categories (
 	id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    name TEXT NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN KEY (user_id) 
+   user_id INTEGER,
+   name TEXT NOT NULL,
+   publish BOOLEAN,
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP,
+   FOREIGN KEY (user_id) 
       REFERENCES users (id) 
          ON DELETE CASCADE 
          ON UPDATE NO ACTION
     
 );
 
+CREATE TABLE IF NOT EXISTS comments (
+	id INTEGER PRIMARY KEY,
+   user_id INTEGER,
+   body TEXT NOT NULL,
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP,
+   FOREIGN KEY (user_id) 
+      REFERENCES users (id) 
+         ON DELETE CASCADE 
+         ON UPDATE NO ACTION
+);
+
 CREATE TABLE IF NOT EXISTS blogs (
 	id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN KEY (user_id) 
+   user_id INTEGER,
+   title TEXT NOT NULL,
+   body TEXT NOT NULL,
+   publish BOOLEAN,
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP,
+   FOREIGN KEY (user_id) 
       REFERENCES users (id) 
          ON DELETE CASCADE 
          ON UPDATE NO ACTION
@@ -42,15 +56,31 @@ CREATE TABLE IF NOT EXISTS category_blogs (
         ON UPDATE NO ACTION
 );
 
+CREATE TABLE IF NOT EXISTS comment_blogs (
+   comment_id INTEGER NOT NULL,
+   blog_id INTEGER NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY (comment_id, blog_id),
+   FOREIGN KEY (comment_id) 
+      REFERENCES comments (id) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION,
+   FOREIGN KEY (blog_id) 
+      REFERENCES blogs (id) 
+        ON DELETE CASCADE 
+        ON UPDATE NO ACTION
+);
+
 CREATE TABLE IF NOT EXISTS profiles(
 	id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    username TEXT NOT NULL,
+   user_id INTEGER,
+   username TEXT NOT NULL,
 	image TEXT,
 	bio TEXT,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    FOREIGN KEY (user_id) 
+   created_at TIMESTAMP,
+   updated_at TIMESTAMP,
+   FOREIGN KEY (user_id) 
       REFERENCES users (id) 
          ON DELETE CASCADE 
          ON UPDATE NO ACTION
@@ -60,6 +90,7 @@ CREATE TABLE IF NOT EXISTS profiles(
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TABLE IF EXISTS comment_blogs;
 DROP TABLE IF EXISTS category_blogs;
 DROP TABLE IF EXISTS blogs;
 DROP TABLE IF EXISTS categories;
